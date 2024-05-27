c3198e32d6e2275a7d4523fecf4d30c4846358fd
ibra8080 <ibrahimabusaif.designer@gmail.com>
2024-05-27 15:26:24 +0000

    Creat request answer function

--- run.py
+++ run.py
@@ -10,4 +10,22 @@ def get_question(ques_num):
     for choice in question_data["choices"]:
         print(choice)
 
-get_question(0)
+
+def request_answer():
+    player_answer = input("Your answer (1, 2, 3, or 4): ").strip()
+
+    if validate_answer(player_answer):
+        print('Data is valid')
+        
+    return player_answer
+
+
+def validate_answer(player_answer):
+    valid_answers = ['1', '2', '3', '4']
+    while player_answer not in valid_answers:
+        print("Invalid input. Please enter 1, 2, 3, or 4.")
+    print(player_answer)
+
+
+get_question(1)
+request_answer()
