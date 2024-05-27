0d8ce56222337bfaa9636f4921ebb43e1f7aeb65
ibra8080 <ibrahimabusaif.designer@gmail.com>
2024-05-27 14:15:59 +0000

    Creat get question function

---
+++ questions.py
@@ -0,0 +1,52 @@
+questions = [
+    {
+        "question": "What is the Arabic word for 'book'?",
+        "choices": ["1. كتاب", "2. قلم", "3. مدرسة", "4. كرسي"],
+        "answer": "1"
+    },
+    {
+        "question": "How do you say 'thank you' in Arabic?",
+        "choices": ["1. مرحبا", "2. وداعا", "3. نعم", "4. شكرا"],
+        "answer": "4"
+    },
+    {
+        "question": "How do you say 'hello' in Arabic?",
+        "choices": ["1. وداعا", "2. من فضلك", "3. مرحبا", "4. نعم"],
+        "answer": "3"
+    },
+    {
+        "question": "What is the Arabic word for 'teacher'?",
+        "choices": ["1. طالب", "2. مدرس", "3. طبيب", "4. مهندس"],
+        "answer": "2"
+    },
+    {
+        "question": "How do you say 'water' in Arabic?",
+        "choices": ["1. شاي", "2. قهوة", "3. ماء", "4. عصير"],
+        "answer": "3"
+    },
+    {
+        "question": "What is the Arabic word for 'sun'?",
+        "choices": ["1. قمر", "2. نجمة", "3. شمس", "4. أرض"],
+        "answer": "3"
+    },
+    {
+        "question": "How do you say 'house' in Arabic?",
+        "choices": ["1. بيت", "2. شارع", "3. مدينة", "4. شجرة"],
+        "answer": "1"
+    },
+    {
+        "question": "How do you say 'friend' in Arabic?",
+        "choices": ["1. عدو", "2. زميل", "3. صديق", "4. أخ"],
+        "answer": "3"
+    },
+    {
+        "question": "How do you say 'good morning' in Arabic?",
+        "choices": ["1. مساء الخير", "2. صباح الخير", "3. تصبح على خير", "4. مع السلامة"],
+        "answer": "2"
+    },
+    {
+        "question": "What is the meaning of 'مساء الخير'?",
+        "choices": ["1. Good morning", "2. Good night", "3. Good evening", "4. Goodbye"],
+        "answer": "3"
+    }
+]
--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcom in ArabiaChallenge \n")
+from questions import questions
+
+def get_question(ques_num):
+    """
+    Get Question from questions sheet
+    """
+    question_data = questions[ques_num]
+    print(question_data["question"])
+    for choice in question_data["choices"]:
+        print(choice)
+
+get_question(0)
