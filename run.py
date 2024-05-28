393b0bc28d996f925055023d8da21b7206a98370
ibra8080 <ibrahimabusaif.designer@gmail.com>
2024-05-28 07:43:23 +0000

    Add score collector to compare answer function

--- run.py
+++ run.py
@@ -26,6 +26,10 @@ def request_answer():
 
 
 def validate_answer(player_answer):
+    """
+    Validate answers, If the input answer not in 1, 2, 3 or 4 rise
+    Invalid input error 
+    """
     valid_answers = ['1', '2', '3', '4']
     if player_answer not in valid_answers:
         print("Invalid input. Please enter 1, 2, 3, or 4.")
@@ -33,19 +37,39 @@ def validate_answer(player_answer):
     return True
 
 
-def compare_answer(player_answer, correct_answer):
+def compare_answer(player_answer, correct_answer, score):
     if player_answer == correct_answer:
+        score += 1
         print("Correct !")
+            
     else:
         print(f"Wrong. The correct answer is {correct_answer}.")
+    print(f"Your Score : {score}")
+    return score
+
+
+# def score_collector(compare_answer, score):
+#     if True:
+#         return 1
+#         print(f"Your Score : {score}")   
+#     else:
+#         return 0
+    
 
 
 def main():
+    """
+    Run all programm functions
+    """
+    score = 0
+
     for i in range(len(questions)):
         question_data = get_question(i)  
         player_answer = request_answer()
-        compare_answer(player_answer, question_data["answer"])
- 
+        score = compare_answer(player_answer, question_data["answer"], score)
+        # score_collector(compare_answer, score)
+        # score = score_collector(compare_answer, score)
+
         
 
 main()
